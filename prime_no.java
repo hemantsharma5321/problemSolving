628c04c8c9575e23adc0d79b4819b7456d562585
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2021-12-19 11:47:18 +0530

    Create prime_no.java

---
+++ prime_no.java
@@ -0,0 +1,19 @@
+public class Prime_check{
+   public static void main(String []args){
+     Scanner sc=new Scanner(System.in);
+     int num=sc.nextInt();
+     int temp=num;
+     for(int i=2;i<=math.sqrt(num);i++){
+      if(num%i==0){
+      System.out.println("not prime");
+      break;
+      }
+      else{
+         System.out.println("prime");
+}
+}
+}
+
+}
+
+}
